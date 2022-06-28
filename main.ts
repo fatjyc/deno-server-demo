24c66461c81e64250f893d64077521c5cc61aa3f
Jiang Yucheng <fatjyc@gmail.com>
2022-06-29 00:43:55 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,5 +2,5 @@ import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
 function handler(_req: Request): Response {
   return new Response("Hello, World!");
 }
-console.log("Listening on http://localhost:8000");
+console.log("Listening  on http://localhost:8000");
 serve(handler);
